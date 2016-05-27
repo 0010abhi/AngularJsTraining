28e63696a62cf3298d123232a9dda631a1db0375
Abhishek Sachdeva <0010abhi@gmail.com>
2016-05-27 22:47:13 +0530

    Removed Console and alerts

--- public/modules/Assignment2/uiRouterProject/controllers/userControl.js
+++ public/modules/Assignment2/uiRouterProject/controllers/userControl.js
@@ -4,22 +4,12 @@
 app.controller("userControl",function($scope,$http){
 	$scope.Message = "User Control";
 	
-	//$scope.dateObj = new Date();
+	$scope.dateObj = new Date();
 
 	var booksFunction = function(){
-		alert("called");
-//		$http.get('http://localhost:3000/userdata.json')
-//		.then(function(sucRes){
-//			console.log(sucRes);
-//			//return sucRes.data;
-//		},function(errRes){
-//			console.log(errRes);
-//			//return sucRes.data;
-//		});	
 		$http.get('http://localhost:3000/userdata.json')
 		.success(function success(sucRes){
 			console.log(sucRes);
-			//return sucRes.data;
 		});	
 	}
 	
